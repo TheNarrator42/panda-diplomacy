fc24935d3d257165c83214609fceda9ee9f0e65f
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2025-04-28 15:01:17 -0400

    Added some sample text to work with

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+# positive sentiment: compound score >= 0.05
+# neutral sentiment: (compound score > -0.05) and (compound score < 0.05)
+# negative sentiment: compound score <= -0.05
 
 if __name__=="__main__":
     InputFile = "input/test.txt"
     OutputFile = "output/test.txt"
 
     with open(InputFile, "r") as f:
-        print(f.read())
-
-    sentences = ["VADER is smart, handsome, and funny."]
+        sentences = f.read().split("\n")
 
     analyzer = SentimentIntensityAnalyzer()
-    for sentence in sentences:
-        vs = analyzer.polarity_scores(sentence)
-        #print("{:-<65} {}".format(sentence, str(vs)))
+    with open(OutputFile, "w") as f:
+        for sentence in sentences:
+            vs = analyzer.polarity_scores(sentence)
+            print("{:-<65} {}".format(sentence, str(vs)))
+            f.write("{:-<65} {}\n".format(sentence, str(vs)))
